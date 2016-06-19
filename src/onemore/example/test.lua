6992452d5d79d637369712f1c5f80d14d7e329ad
onemore <treert@sina.com>
2016-06-19 23:23:36 +0800

    code生成看了个大概 发现个函数调用的bug,修复掉

--- src/onemore/example/test.lua
+++ src/onemore/example/test.lua
@@ -1,11 +1,67 @@
 
+
+
+f1 = function (a,b)
+    print(type(a).."x"..type(b))
+end
+
+f1({2})
+
+
 local function test_repeat_local()
     local a = 1
     local a = 2
     print(a.."")
 end
 
+function print_tab(deep)
+    deep = deep or 0
+    for i=1,deep,1 do
+        puts("  ")
+    end
+end
 
+function tostring(a)
+    if type(a) == "string" then
+        return '"'..a..'"'
+    end
+    if type(a) == "number" then
+        return a..""
+    end
+    
+    return type(a)
+end
+
+function dump(a,deep)
+    deep = deep or 0
+    if type(a) == "table" then
+        print("{")
+        for k,v in pairs(a) do
+            print_tab(deep+1)
+            puts("["..tostring(k).."] = ")
+            dump(v,deep+1)
+            print(",")
+        end
+        print_tab(deep)
+        puts("}")
+        return
+    end
+
+    puts(tostring(a))
+end
+
+function get_table(...)
+
+    return {1,2,...}
+end
+
+
+dump({
+    1,2,3,get_table("a","b"),4,6
+    })
+
+    --[[
+--dump(get_table("a","b"))
 
 while true do
     puts("exit > ")
@@ -14,6 +70,7 @@ while true do
     if line == "exit" then
         break
     end
-    
-    test_repeat_local()()
 end
+
+
+--]]
